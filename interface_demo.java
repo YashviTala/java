ef95d108f2d829b3ca8ca7f2a24766cc28e982e1
YashviTala <yashvitala@gmail.com>
2026-08-14 08:31:31 +0530

    Add files via upload

---
+++ interface_demo.java
@@ -0,0 +1,28 @@
+interface a 
+{
+ void draw();
+}
+ interface b extends a
+{
+ void show();
+}
+class xyz implements b
+{
+ public void draw()
+{
+ System.out.println("drawing circle");
+}
+public void show()
+{
+ System.out.println("show method");
+}
+}
+class interface_demo
+{
+ public static void main (string args[])
+ {
+  xyz obj=new xyz();
+  obj.draw();
+  obj.show();
+  }
+ } 
