49de959182452f19fd707a3a461856b2cbf24af9
YashviTala <yashvitala@gmail.com>
2026-08-21 08:29:23 +0530

    Add files via upload

---
+++ try catch.java
@@ -0,0 +1,19 @@
+public class trycatch ex
+{
+  public static void main(String[]args)
+  {
+    int i=50;
+	int j=0;
+	int data;
+	try
+	{
+	  data=i/j;
+	}
+	catch(exception e)
+	{
+	 "resolving the excepion in catch block"
+	}
+     system.out.println(i/j+2));
+    }
+   }
+}   
